326aaff7b0676eb0765d780a76f2780e8f463403
Arman Galyan <agalyan@ymail.com>
2021-02-14 22:02:30 +0400

    Added some missed fields to fix the remote build for deployment.

--- config/site.js
+++ config/site.js
@@ -1,6 +1,8 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'VisualCV Questionnaire', // Navigation and Site Title
+  url: 'https://www.visualcv.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://www.visualcv.com/', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
